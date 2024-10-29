0dff72ed37db74bf5041cf84e65dd9878632c998
Giuseppe <giu_chie@hotmail.it>
2024-10-29 13:59:16 +0100

    setup eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import localFont from "next/font/local";
+import React from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
