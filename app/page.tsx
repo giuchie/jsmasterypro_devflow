78f965fb9562db29604b9400c38fa6afbc016feb
Giuseppe <giu_chie@hotmail.it>
2024-10-29 15:29:09 +0100

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">DevOverflow</h1>
+      <h1 className="text-primary text-3xl font-bold">DevOverflow</h1>
     </div>
   );
 };
